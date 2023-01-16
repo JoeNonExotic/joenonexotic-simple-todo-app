80ca2eb5397b77ab8aeef90d9f9af5715ff08de1
JoeNonExotic <vijayvikramreddy@gmail.com>
2023-01-15 16:56:22 -0800

    experiment with gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import TodoContainer from './todo-components/function-components/ToDoContainer';
 import "./todo-components/class-components/App.css";
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import "./todo-components/function-components/App.css";
 
 const root = ReactDOM.createRoot(
